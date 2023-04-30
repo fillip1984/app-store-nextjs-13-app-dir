51f8937a7b82daf8a3a544eb60426f53ec9edf5d
fillip1984 <fillip1984@gmail.com>
2023-04-30 13:36:56 -0400

    updated gitignore to ignore local sqlite db files also generated prisma client and migrated to db

---
+++ prisma/migrations/20230430173407_initial_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
